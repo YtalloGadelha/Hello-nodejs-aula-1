a1908334dec020f481a5048bd0c6d059c2db3255
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-11-10 12:06:09 -0300

    Adição do arquivo Lista e mapa

---
+++ Listas e mapas.js
@@ -0,0 +1,26 @@
+//Listas e mapas
+
+let lista = [];
+lista.push(1);
+
+lista.push(10);
+
+lista.push(100);
+
+lista.shift();
+
+lista.shift();
+
+lista.push(10);
+
+lista.push(1);
+
+console.log(lista);
+
+let mapa = {"Nome" : "Ytallo"};
+mapa["Cidade"] = "Fortaleza";
+mapa["Estado"] = "Ceará";
+
+console.log(mapa);
+
+console.log(mapa.Cidade);
